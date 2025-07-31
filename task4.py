13285e2686cb0a05064a5162493b37aea27a2af4
Nethra2008 <nethra78716@gmail.com>
2025-07-31 15:51:58 +0530

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,10 @@
+task1.py
+num=int(input("Enter a mark"))
+if num>=90:
+  print("grade A")
+elif num>=80 or num<90:
+  print("Grade B")
+elif num>=50 or num<90:
+  print("Grade C")
+else:
+  print("Grade D") 
