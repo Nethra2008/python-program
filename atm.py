d5bb229c467df35e50a4afa673374042324b5ac9
Nethra2008 <nethra78716@gmail.com>
2025-07-31 15:49:25 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,9 @@
+atm.py
+user_Pin=input("Enter your 4-digit ATM Pin:")
+if "CheckBalance or withdraw":
+  print("Welcome to ATM")
+  print("ATM menu")
+  print("Check balance")
+  print("Deposit")
+  print("Withdraw")
+  print("Exit")
